38f6508ce1802f888c0ec4d0537add78535b8fae
Vladyslav Hryhorenko <hryhorenkovladyslav@outlook.com>
2025-07-02 16:20:31 +0300

    add libfido2

--- install/xtras.sh
+++ install/xtras.sh
@@ -1,6 +1,7 @@
 yay -S --noconfirm --needed \
   spotify zoom \
-  obsidian libreoffice obs-studio
+  obsidian libreoffice obs-studio \
+  libfido2
 
 # Copy over Witchcraft applications
 source ~/.local/share/witchcraft/bin/witchcraft-sync-applications
