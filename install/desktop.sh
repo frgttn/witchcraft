bba9d05527f41c79205e9987b7f5451d8d9b73e5
Vladyslav Hryhorenko <hryhorenkovladyslav@outlook.com>
2025-07-05 17:57:29 +0300

    add ffmpegthumbnailer

--- install/desktop.sh
+++ install/desktop.sh
@@ -2,6 +2,6 @@ yay -S --noconfirm --needed \
   brightnessctl ddcutil playerctl pamixer pavucontrol wireplumber \
   fcitx5 fcitx5-gtk fcitx5-qt fcitx5-configtool \
   wl-clip-persist clipse-bin \
-  nautilus sushi gnome-calculator \
+  nautilus sushi ffmpegthumbnailer gnome-calculator \
   nwg-displays nwg-look \
   chromium mpv evince imv 
