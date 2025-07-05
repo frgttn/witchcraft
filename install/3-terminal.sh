85dc81ccf5bab0a1bf2de762a5a18ce6a10e169a
Vladyslav Hryhorenko <hryhorenkovladyslav@outlook.com>
2025-07-06 00:17:52 +0300

    input changes

--- install/3-terminal.sh
+++ install/3-terminal.sh
@@ -2,6 +2,6 @@ paru -S --noconfirm --needed \
   wget curl nano unzip inetutils \
   fd eza fzf ripgrep zoxide bat \
   wl-clipboard fastfetch btop \
-  man tldr less whois plocate \
+  man tldr less whois plocate jq \
   alacritty bash-completion \
   lm_sensors libfido2
