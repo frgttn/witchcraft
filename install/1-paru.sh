0ba8cc5da43f0c1b1f6a659f8fa518060830e41a
Vladyslav Hryhorenko <hryhorenkovladyslav@outlook.com>
2025-07-07 21:01:53 +0300

    changes with impala

--- install/1-paru.sh
+++ install/1-paru.sh
@@ -1,9 +1,11 @@
 sudo pacman -S --needed --noconfirm base-devel
 
-if ! command -v paru &>/dev/null; then
+if ! command -v yay &>/dev/null; then
+  cd /tmp
   git clone https://aur.archlinux.org/paru.git
   cd paru
   makepkg -si --noconfirm
   cd ~
+  cd -
   rm -rf paru
-fi
+  cd ~
