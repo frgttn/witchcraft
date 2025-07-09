af35132dc11d037f0de7264cd4a35966ae6f8904
Vladyslav Hryhorenko <hryhorenkovladyslav@outlook.com>
2025-07-09 11:56:06 +0300

    hotfix

--- install/theme.sh
+++ install/theme.sh
@@ -7,6 +7,7 @@ gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark"
 gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
 
 # Set initial wallpaper
+mkdir -p ~/.config/witchcraft/current
 ln -snf ~/.local/share/witchcraft/backgrounds/1.png ~/.config/witchcraft/current/background
 wal -i ~/.config/witchcraft/current/background -t -s
 
