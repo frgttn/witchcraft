f89ca1fd333ff66970ec902ce5b3baa06016fcaa
Vladyslav Hryhorenko <hryhorenkovladyslav@outlook.com>
2025-07-09 23:19:54 +0300

    add toggle idle

--- install/desktop.sh
+++ install/desktop.sh
@@ -1,7 +1,7 @@
 paru -S --noconfirm --needed \
   brightnessctl ddcutil playerctl pamixer pavucontrol wireplumber \
   fcitx5 fcitx5-gtk fcitx5-qt fcitx5-configtool \
-  wl-clip-persist clipse-bin \
+  wl-clip-persist clipse-bin gnome-keyring \
   nautilus sushi ffmpegthumbnailer gnome-calculator \
   nwg-displays nwg-look \
   chromium mpv evince imv 
